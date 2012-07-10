ed1d902ef3a9ceb9d7b6dafd0fdb36da0858dead
Cody Caughlan <toolbag@gmail.com>
2012-07-09 18:51:42 -0700

    Respect paging parameters in Item and Customer list endpoints. Version bump for gem release

--- lib/quickeebooks/windows/service/customer.rb
+++ lib/quickeebooks/windows/service/customer.rb
@@ -7,7 +7,11 @@ module Quickeebooks
       class Customer < Quickeebooks::Windows::Service::ServiceBase
 
         def list(filters = [], page = 1, per_page = 20, sort = nil, options = {})
-          fetch_collection("customer", "Customer", Quickeebooks::Windows::Model::Customer, nil, filters, page, per_page, sort, options)
+          custom_field_query = '<?xml version="1.0" encoding="utf-8"?>'
+          custom_field_query += '<CustomerQuery xmlns="http://www.intuit.com/sb/cdm/v2">'
+          custom_field_query += "<StartPage>#{page}</StartPage><ChunkSize>#{per_page}</ChunkSize>"
+          custom_field_query += '</CustomerQuery>'
+          fetch_collection("customer", "Customer", Quickeebooks::Windows::Model::Customer, custom_field_query.strip, filters, page, per_page, sort, options)
         end
 
       end
