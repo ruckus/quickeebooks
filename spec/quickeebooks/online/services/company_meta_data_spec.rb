22bcce2abb8ad9c791b7ddb07c7d7daef45fa391
Walter McGinnis <wm@waltermcginnis.com>
2012-07-14 12:32:43 +1200

    [#32491883] Added CompanyMetaData service and model in support of getting demographic data about an organization connected to Quickbooks.
    Based on https://ipp.developer.intuit.com/0010_Intuit_Partner_Platform/0050_Data_Services/0400_QuickBooks_Online/CompanyMetaData.

---
+++ spec/quickeebooks/online/services/company_meta_data_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+require "fakeweb"
+require "oauth"
+require "quickeebooks"
+
+describe "Quickeebooks::Online::Service::CompanyMetaData" do
+  before(:all) do
+    FakeWeb.allow_net_connect = false
+    qb_key = "key"
+    qb_secret = "secreet"
+
+    @realm_id = "9991111222"
+    @base_uri = "https://qbo.intuit.com/qbo36"
+    @oauth_consumer = OAuth::Consumer.new(qb_key, qb_key, {
+        :site                 => "https://oauth.intuit.com",
+        :request_token_path   => "/oauth/v1/get_request_token",
+        :authorize_path       => "/oauth/v1/get_access_token",
+        :access_token_path    => "/oauth/v1/get_access_token"
+    })
+    @oauth = OAuth::AccessToken.new(@oauth_consumer, "blah", "blah")
+  end
+
+  it "can get the realm's company_meta_data record" do
+    xml = File.read(File.dirname(__FILE__) + "/../../../xml/online/company_meta_data.xml")
+    service = Quickeebooks::Online::Service::CompanyMetaData.new(@oauth, @realm_id, @base_uri)
+    url = "#{service.url_for_resource("companymetadata")}"
+    FakeWeb.register_uri(:get, url, :status => ["200", "OK"], :body => xml)
+    company_meta_data = service.company_meta_data
+    company_meta_data.registered_company_name.should == "Bay Area landscape services"
+  end
+end
