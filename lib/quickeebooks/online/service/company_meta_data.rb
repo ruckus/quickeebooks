5235b76eb2b66c10713f1764f94285238d64c961
Walter McGinnis <wm@waltermcginnis.com>
2012-07-14 12:32:43 +1200

    [#32491883] Added CompanyMetaData service and model in support of getting demographic data about an organization connected to Quickbooks.
    Based on https://ipp.developer.intuit.com/0010_Intuit_Partner_Platform/0050_Data_Services/0400_QuickBooks_Online/CompanyMetaData.

---
+++ lib/quickeebooks/online/service/company_meta_data.rb
@@ -0,0 +1,22 @@
+require 'quickeebooks/online/service/service_base'
+require 'nokogiri'
+
+module Quickeebooks
+  module Online
+    module Service
+      # read only, corresponds to the realm_id
+      # see https://ipp.developer.intuit.com/0010_Intuit_Partner_Platform/0050_Data_Services/0400_QuickBooks_Online/CompanyMetaData
+      class CompanyMetaData < ServiceBase
+        def company_meta_data
+          url = "#{url_for_resource("companymetadata")}"
+          response = do_http_get(url)
+          if response && response.code.to_i == 200
+            Quickeebooks::Online::Model::CompanyMetaData.from_xml(response.body)
+          else
+            nil
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/quickeebooks/online/company_meta_data_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+require "quickeebooks/online/model/company_meta_data"
+
+describe "Quickeebooks::Online::Model::CompanyMetaData" do
+  
+  it "parse customer from XML" do
+    xml = File.read(File.dirname(__FILE__) + "/../../xml/online/company_meta_data.xml")
+    company_meta_data = Quickeebooks::Online::Model::CompanyMetaData.from_xml(xml)
+
+    company_meta_data.external_realm_id.should be_nil
+
+    company_meta_data.registered_company_name.should == "Bay Area landscape services"
+
+    company_meta_data.industry_type.should == "Landscaping Services"
+
+    company_meta_data.addresses.count.should == 2
+    
+    company_meta_data.addresses.first.line1.should == "2600 Service Street"
+
+    company_meta_data.legal_address.line1.should == "2602 Service Street"
+    
+    company_meta_data.phone.free_form_number.should == "(669)111-2222"
+    
+    company_meta_data.emails.size.should == 2
+    company_meta_data.emails.first.address.should == "john@bayarealandscapeservices.com"
+    
+    # Tag not supported yet in Quickeebooks::Online::Model::Email
+    # company_meta_data.emails.first.tag.should == "COMPANY_EMAIL"
+  end
+end
