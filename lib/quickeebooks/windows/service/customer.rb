ee9a2e77b2509efdb4ead28b49f8a2259732d289
Chris Dosé <cdose@immense.net>
2012-12-03 10:26:39 -0600

    When saving customers with tax fields, set ids to nil

--- lib/quickeebooks/windows/service/customer.rb
+++ lib/quickeebooks/windows/service/customer.rb
@@ -12,15 +12,15 @@ module Quickeebooks
           custom_field_query += '</CustomerQuery>'
           fetch_collection(Quickeebooks::Windows::Model::Customer, custom_field_query.strip, filters, page, per_page, sort, options)
         end
-        
+
         def fetch_by_id(id, idDomain = 'QB', options = {})
           url = "#{url_for_resource(Quickeebooks::Windows::Model::Customer::REST_RESOURCE)}/#{id}"
           fetch_object(Quickeebooks::Windows::Model::Customer, url, {:idDomain => idDomain})
         end
-        
+
         def create(customer)
           raise InvalidModelException unless customer.valid_for_create?
-          
+
           # XML is a wrapped 'object' where the type is specified as an attribute
           #    <Object xsi:type="Invoice">
           xml_node = customer.to_xml(:name => 'Object')
@@ -33,31 +33,33 @@ module Quickeebooks
           XML
           perform_write(Quickeebooks::Windows::Model::Customer, xml)
         end
-        
+
         def update(customer)
           # XML is a wrapped 'object' where the type is specified as an attribute
           #    <Object xsi:type="Invoice">
-          
+
           # Intuit requires that some fields are unset / do not exist.
           customer.meta_data = nil
           customer.external_key = nil
+          customer.tax_id = nil
+          customer.sales_tax_code_id = nil
 
-          # unset Id fields in addresses, phones, email
+          # unset Id fields in addresses, phones, email, and website
           if customer.addresses
             customer.addresses.each {|address| address.id = nil }
           end
           if customer.email
             customer.email.id = nil
           end
-          
+
           if customer.phones
             customer.phones.each {|phone| phone.id = nil }
           end
-          
+
           if customer.web_site
             customer.web_site.id = nil
           end
-          
+
           xml_node = customer.to_xml(:name => 'Object')
           xml_node.set_attribute('xsi:type', 'Customer')
           xml = <<-XML
