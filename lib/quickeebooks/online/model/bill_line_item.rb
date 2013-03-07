74b1b7fc85d4e43d8820736333ee8eed68c913ab
Christian <zarski@gmail.com>
2013-03-07 08:26:57 -0500

    map quantity not qty

--- lib/quickeebooks/online/model/bill_line_item.rb
+++ lib/quickeebooks/online/model/bill_line_item.rb
@@ -12,7 +12,7 @@ module Quickeebooks
         xml_accessor :billable_status,            :from => 'BillableStatus'
         xml_accessor :item_id,                    :from => 'ItemId',                  :as => Quickeebooks::Online::Model::Id
         xml_accessor :unit_price,                 :from => 'UnitPrice',               :as => Float
-        xml_accessor :qty,                        :from => 'Qty',                     :as => Float
+        xml_accessor :quantity,                   :from => 'Qty',                     :as => Float
         xml_accessor :override_item_account_id,   :from => 'OverrideItemAccountId',   :as => Quickeebooks::Online::Model::Id
         xml_accessor :account_id,                 :from => 'AccountId',               :as => Quickeebooks::Online::Model::Id
         xml_accessor :reimbursable,               :from => 'ReimbursableInfo',        :as => Quickeebooks::Online::Model::ReimbursableInfo
