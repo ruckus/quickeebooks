6716b3c50327abd6c61de46cf81a6a999f66b7f1
Elliot Winkler <elliot.winkler@gmail.com>
2013-10-17 22:42:19 -0600

    Fix failing test on Ruby 2.0.0
    It seems that the behavior of CGI.escapeHTML was changed in Ruby 2, to
    where it now escapes apostrophes.

--- spec/lib/quickeebooks/shared/service/filter_spec.rb
+++ spec/lib/quickeebooks/shared/service/filter_spec.rb
@@ -102,7 +102,7 @@ describe "Quickeebooks::Shared::Service::Filter" do
       it 'CGI escapes values' do
         filter.new(:text,
           :field => 'Foo',
-          :value => "<3 Bar's ><things").to_xml.should == '<Foo>&lt;3 Bar\'s &gt;&lt;things</Foo>'
+          :value => "<3 Bar ><things").to_xml.should == '<Foo>&lt;3 Bar &gt;&lt;things</Foo>'
       end
 
       it 'CGI escapes integers' do
