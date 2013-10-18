74a449746becc1f69af13716c16725c752221633
Elliot Winkler <elliot.winkler@gmail.com>
2013-10-17 23:15:36 -0600

    Move methods defined in spec/support to modules
    It is ill-advised to create global methods even if they are only defined
    in test-land.

---
+++ spec/support/fixture_helpers.rb
@@ -0,0 +1,21 @@
+module FixtureHelpers
+  def fixture_path
+    File.expand_path("../../xml", __FILE__)
+  end
+
+  def onlineFixture(file)
+    File.new(fixture_path + '/online/' + file).read
+  end
+
+  def sharedFixture(file)
+    File.new(fixture_path + '/shared/' + file).read
+  end
+
+  def windowsFixture(file)
+    File.new(fixture_path + '/windows/' + file).read
+  end
+end
+
+RSpec.configure do |config|
+  config.include(FixtureHelpers)
+end
