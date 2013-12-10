58d53faf70d8bb434cf07decebeac2ed3e27ebe0
Christian <zarski@gmail.com>
2013-12-10 17:06:23 -0500

    Ensure that a line item is initialized

--- lib/quickeebooks/windows/model/credit_memo.rb
+++ lib/quickeebooks/windows/model/credit_memo.rb
@@ -1,34 +1,49 @@
-require 'quickeebooks'
 require 'quickeebooks/windows/model/id'
-require 'quickeebooks/windows/model/external_key'
+require 'quickeebooks/windows/model/credit_memo_header'
+require 'quickeebooks/windows/model/credit_memo_line_item'
+require 'quickeebooks/windows/model/address'
 require 'quickeebooks/windows/model/meta_data'
+require 'quickeebooks/windows/model/tax_line'
 
 module Quickeebooks
   module Windows
     module Model
       class CreditMemo < Quickeebooks::Windows::Model::IntuitType
+        include ActiveModel::Validations
 
-    		XML_COLLECTION_NODE = 'CreditMemo'
-    		XML_NODE = 'CreditMemo'
+        XML_COLLECTION_NODE = 'CreditMemos'
+        XML_NODE = 'CreditMemo'
 
-    		# https://services.intuit.com/sb/creditmemo/v2/<realmID>
-    		REST_RESOURCE = "creditmemo"
+        # https://services.intuit.com/sb/creditmemo/v2/<realmID>
+        REST_RESOURCE = "creditmemo"
 
-    		xml_convention :camelcase
-        xml_accessor :id, :as => Quickeebooks::Windows::Model::Id
-        xml_accessor :meta_data, :from => 'MetaData', :as => Quickeebooks::Windows::Model::MetaData
-        xml_accessor :external_key, :as => Quickeebooks::Windows::Model::ExternalKey
-        xml_accessor :synchronized
+        xml_convention :camelcase
+        xml_accessor :id, :from => 'Id', :as => Quickeebooks::Windows::Model::Id
         xml_accessor :sync_token, :from => 'SyncToken', :as => Integer
+        xml_accessor :meta_data, :from => 'MetaData', :as => Quickeebooks::Windows::Model::MetaData
+        xml_accessor :external_key, :from => 'ExternalKey'
+        xml_accessor :synchronized, :from => 'Synchronized'
+        xml_accessor :custom_fields, :from => 'CustomField', :as => [Quickeebooks::Windows::Model::CustomField]
+        xml_accessor :draft
+        xml_accessor :object_state, :from => 'ObjectState'
         xml_accessor :header, :from => 'Header', :as => Quickeebooks::Windows::Model::CreditMemoHeader
         xml_accessor :line_items, :from => 'Line', :as => [Quickeebooks::Windows::Model::CreditMemoLineItem]
         xml_accessor :tax_line, :from => 'TaxLine', :as => Quickeebooks::Windows::Model::TaxLine
 
-        def valid_for_create?
-          true
+        validates_length_of :line_items, :minimum => 1
+
+        def initialize
+          ensure_line_items_initialization
+        end
+
+        private
+
+        def ensure_line_items_initialization
+          self.line_items ||= []
         end
 
       end
     end
   end
+
 end
